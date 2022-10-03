43a3e1e2b4ad3ae899cc54904e7c6e230d0722b6
Shiraz Esat <shiraz.esat@gmail.com>
2022-10-03 18:02:49 +0100

    Basic CRUD for tasks
    Used template files
    Controller -> View

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,14 +7,51 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  def new; end
+  def new
+    count = Task.count + 1
+    @task = Task.new(position: count)
+  end
 
-  def create; end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render('new')
+    end
+  end
 
-  def edit; end
+  def edit
+    @task = Task.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render('edit')
+    end
+  end
 
-  def delete; end
-  def destroy; end
+  def delete
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :position,
+      :completed,
+      :description
+    )
+  end
 end
